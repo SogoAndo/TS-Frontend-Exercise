02f6f71d69e9015d10be5cc3b711216611ca84b1
SogoAndo <s-ando@coel-inc.jp>
2026-06-25 14:44:47 +0900

    演習4-2を実装

--- app/layout/basic.tsx
+++ app/layout/basic.tsx
@@ -36,6 +36,8 @@ export default function RootLayout({
               <a href="/chapter03/ex3-3" className="hover:underline">商品一覧</a>
               {/* 演習 4-1 計算機を作成する(加算機能のみ) */}
               <a href="/chapter04/ex4-1" className="hover:underline">計算機</a>
+              {/* 演習 4-2 計算の種類をプルダウンで選択可能にする */}
+              <a href="/chapter04/ex4-2" className="hover:underline">計算機(プルダウン)</a>
             </nav>
           </div>
         </header>
